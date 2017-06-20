7a4ad048de794a0685e12fa5316c03d9bbde8b9e
liuzechen <1040981145@qq.com>
2017-06-20 16:13:22 +0800

    AppConfig

---
+++ JZCommonService/Classes/JZAppConfig.h
@@ -0,0 +1,15 @@
+//
+//  JZAppConfig.h
+//  JZCommonService
+//
+//  Created by jzzx on 2017/6/20.
+//  Copyright © 2017年 Zechen Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JZAppConfig : NSObject
+
++ (void)init;
+
+@end
---
+++ JZCommonService/Classes/JZAppConfig.m
@@ -0,0 +1,21 @@
+//
+//  JZAppConfig.m
+//  JZCommonService
+//
+//  Created by jzzx on 2017/6/20.
+//  Copyright © 2017年 Zechen Liu. All rights reserved.
+//
+
+#import "JZAppConfig.h"
+
+@implementation JZAppConfig
+
++ (void)init {
+    [self appearance];
+}
+
++ (void)appearance {
+    NSLog(@"外观配置");
+}
+
+@end
